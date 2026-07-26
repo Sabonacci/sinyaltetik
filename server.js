f234503599b4646bd1fc716a02a9a721e00a9ec4
Sabonacci <sabansah0041@gmail.com>
2026-07-26 21:31:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const TELEGRAM_CHAT_ID = '5756145019'
 
 // Takip edilecek hisseler
 const HISSELER = [
-  'EREGL.IS', 'ARFYE.IS', 'ARDYZ.IS', 'ORCAY.IS', 'OBAMS.IS', 'BIOEN.IS', 'SASA.IS',
+  'EREGL.IS', 'ARFYE.IS', 'ARDYZ.IS', 'ORCAY.IS', 'OBAMS.IS', 'BIOEN.IS', 'CIMSA.IS',
   'THYAO.IS', 'ASELS.IS', 'SISE.IS', 'ENJSA.IS', 'GESAN.IS', 'PCILT.IS', 'TRMET.IS',
 ]
 
